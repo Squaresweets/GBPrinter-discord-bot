a4d88d9178c70b7135528e0d4ee2bc03da7fbd40
Matthew Collier <matthew@matthewcollier.co.uk>
2022-03-01 17:58:29 +0000

    Update serialCommunication.py

--- serialCommunication.py
+++ serialCommunication.py
@@ -5,28 +5,39 @@ import sys
 import time
 import os
 
+INQU = bytearray(b'\x88\x33\x0F\x00\x00\x00\x0F\x00\x00')
+INIT = bytearray(b'\x88\x33\x01\x00\x00\x00\x01\x00\x00\x00')
+PRNT = bytearray(b'\x88\x33\x02\x00\x04\x00\x01\x00\xE4\x40\x2B\x01\x00\x00')
+EMPT = bytearray(b'\x88\x33\x04\x00\x00\x00\x04\x00\x00\x00')
+DATA_header = bytearray(b'\x88\x33\x04\x00\x80\x02')
+
+
 def exit_gracefully():
     if dev is not None:
         usb.util.dispose_resources(dev)
         if reattach:
             dev.attach_kernel_driver(0)
-    print('Done.')
+
 
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!')
     exit_gracefully()
 
-signal.signal(signal.SIGINT, signal_handler)
 
+def send(data):
+    print("SENDING: ", end="")
+    for b in data:
+        print("0x%02x " % b, end="")
+        epOut.write(b.to_bytes(1, byteorder='big'))
+    print("")
+
+
+signal.signal(signal.SIGINT, signal_handler)
 
 dev = None
 devices = list(usb.core.find(find_all=True, idVendor=0xcafe, idProduct=0x4011))
 for d in devices:
-    print('Device: %s' % d.product)
-    #if d.product.endswith('PRIMARY'):
     dev = d
-
-#dev = usb.core.find(idVendor=0xcafe, idProduct=0x4011)
 
 if dev is None:
     raise ValueError('Device not found')
@@ -51,8 +62,6 @@ cfg = dev.get_active_configuration()
 
 intf = cfg[(2,0)]   # Or find interface with class 0xff
 
-#print('Interface: %s' % intf)
-
 epIn = usb.util.find_descriptor(
     intf,
     custom_match = \
@@ -61,8 +70,6 @@ epIn = usb.util.find_descriptor(
         usb.util.ENDPOINT_IN)
 
 assert epIn is not None
-
-#print('EP In: %s' % epIn)
 
 epOut = usb.util.find_descriptor(
     intf,
@@ -74,19 +81,15 @@ epOut = usb.util.find_descriptor(
 
 assert epOut is not None
 
-#print('EP Out: %s' % epOut)
-
 
 # Control transfer to enable webserial on device
 print("control transfer out...")
 dev.ctrl_transfer(bmRequestType = 1, bRequest = 0x22, wIndex = 2, wValue = 0x01)
 
-data = bytearray(b'\x88\x33\x0F\x00\x00\x00\x0F\x00\x00')
-# GB Printer commands
-for b in data:
-    print("SEND: 0x%02x" % b)
-    epOut.write(b.to_bytes(1, byteorder='big'))
-    recv = epIn.read(epIn.wMaxPacketSize, 100)
-    print("recv from secondary: 0x%02x" % int.from_bytes(recv, byteorder='big'))
+send(INQU)
+recv = int.from_bytes(epIn.read(epIn.wMaxPacketSize, 100), byteorder='little')
+if recv == 0x81:
+    print("Printer is there :D")
+
 
 exit_gracefully()
