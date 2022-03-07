a1cad6a8b71ff9355c8853348f85679f8b7eaaba
Matthew Collier <matthew@matthewcollier.co.uk>
2022-03-07 16:56:38 +0000

    Some more changes, not much is working. have an idea tho

--- serialCommunication.py
+++ serialCommunication.py
@@ -14,12 +14,12 @@ PRNT = bytearray(b'\x88\x33\x02\x00\x04\x00\x01\x00\xE4\x40\x2B\x01\x00\x00')
 EMPT = bytearray(b'\x88\x33\x04\x00\x00\x00\x04\x00\x00\x00')
 DATA_header = bytearray(b'\x88\x33\x04\x00\x80\x02')
 
-
 def exit_gracefully():
     if dev is not None:
         usb.util.dispose_resources(dev)
         if reattach:
             dev.attach_kernel_driver(0)
+    exit()
 
 
 def signal_handler(sig, frame):
@@ -27,19 +27,25 @@ def signal_handler(sig, frame):
     exit_gracefully()
 
 
-# def send(data):
-#     print("SENDING: ", end="")
-#     for b in data:
-#         print("0x%02x " % b, end="")
-#         epOut.write(b.to_bytes(1, byteorder='big'))
-#     print("")
-
 def send(data):
     print("SENDING: ", end="")
     for b in data:
         print("0x%02x " % b, end="")
-    epOut.write(data)
+        epOut.write(b.to_bytes(1, byteorder='big'))
     print("")
+
+# def send(data):
+#     print("SENDING: ", end="")
+#     for b in data:
+#         print("0x%02x " % b, end="")
+#     epOut.write(data)
+#     print("")
+
+
+def read():
+    recv = int.from_bytes(epIn.read(epIn.wMaxPacketSize, 100), byteorder='big')
+    print("0x%02x " % recv)
+    return recv
 
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -94,22 +100,11 @@ assert epOut is not None
 # Control transfer to enable webserial on device
 dev.ctrl_transfer(bmRequestType = 1, bRequest = 0x22, wIndex = 2, wValue = 0x01)
 
-send(INQU)
-recv = int.from_bytes(epIn.read(epIn.wMaxPacketSize, 10000), byteorder='little')
-print("0x%02x " % recv)
-if recv == 0x81:
-    print("Printer is there :D")
-else:
-    print("Printer is not there :(")
-    exit()
+
 
 # Load image from before
 image = Image.open("dithered.png")
 img = np.array(ImageOps.fit(image, (160, 144)))
-
-send(INIT)
-
-c = 0
 # create the byte array to store the entire image
 data = bytearray()
 for y in range(144):
@@ -120,6 +115,23 @@ for y in range(144):
         byte = byte + (int(img[y][x+2] / 64) << 2)
         byte = byte + int(img[y][x+3] / 64)
         data.append(byte)
+
+
+
+
+send(INQU)
+recv = read()
+if recv == 0x8100:
+    print("Printer is there :D")
+else:
+    print("Printer is not there :(")
+    #exit_gracefully()
+
+
+send(INIT)
+read()
+
+c = 0
 # Send the data over for each of the 9 sections of the image
 for i in range(9):
     DATA_SD = bytearray(649)
@@ -136,22 +148,14 @@ for i in range(9):
     DATA_SD.append(0)
     DATA_SD.append(0)
     send(DATA_SD)
+    read()
 
 send(INQU)
-recv = int.from_bytes(epIn.read(1000, 100), byteorder='little')
-print("0x%02x " % recv)
-recv = int.from_bytes(epIn.read(1000, 100), byteorder='little')
-print("0x%02x " % recv)
+read()
 send(PRNT)
-recv = int.from_bytes(epIn.read(1000, 100), byteorder='little')
-print("0x%02x " % recv)
-recv = int.from_bytes(epIn.read(1000, 100), byteorder='little')
-print("0x%02x " % recv)
+read()
 send(INQU)
-recv = int.from_bytes(epIn.read(1000, 100), byteorder='little')
-print("0x%02x " % recv)
-recv = int.from_bytes(epIn.read(1000, 100), byteorder='little')
-print("0x%02x " % recv)
+read()
 
 while True:
     send(INQU)
