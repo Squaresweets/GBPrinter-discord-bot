cf77a2719e50856fed45612028af32fc04436fad
FerdinandoPH <perez.holguin@gmail.com>
2022-06-11 19:55:48 +0200

    repurposed for printing pictures from a computer

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-import discord
 from PIL import Image
 import ImageProcessing
 import serialCommunication
 import os
-from dotenv import load_dotenv
-image_types = ["png", "jpeg", "jpg"]
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-
-client = discord.Client()
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    for attachment in message.attachments:
-        if any(attachment.filename.lower().endswith(image) for image in image_types):
-            await attachment.save("image.png")
-            ImageProcessing.process_image('image.png')
-            await message.channel.send(file=discord.File(r"dithered.png"))
-            serialCommunication.printImage()
-client.run(TOKEN)
+from tkinter import filedialog
+current_dir = os.path.dirname(os.path.abspath(__file__))
+filearchivo=filedialog.askopenfilename(initialdir = current_dir,title = "Selecciona una imagen",filetypes = (("png files","*.png"),("jpg files","*.jpg"),("all files","*.*")))
+img = Image.open(filearchivo)
+imgpng=img.save(current_dir+'\\image.png')
+ImageProcessing.process_image('image.png')
+serialCommunication.printImage()
